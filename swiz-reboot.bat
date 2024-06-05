f86e7b5a639c547fb295269332dd80f08580a96f
Xi Guan Theng <65259543+aanset@users.noreply.github.com>
2024-06-05 11:23:10 +0700

    Update swiz-reboot.bat

--- swiz-reboot.bat
+++ swiz-reboot.bat
@@ -1,18 +1,35 @@
-@ECHO Off
-    SET /P yesno=This computer will be reboot in 5 minutes. Do you want to Proceed? [Y/N]:
+@echo off
+REM This script to reboot computer every Friday at specific time.
+REM Created by Support Team - AS
+
+SET /P yesno=Hello, this is part of weekly maintenance. This computer will restart in 5 minutes. Do you want to Proceed? [Y/N]:
     IF "%yesno%"=="y" GOTO Confirmation
     IF "%yesno%"=="Y" GOTO Confirmation
     IF "%yesno%"=="n" GOTO End
     IF "%yesno%"=="N" GOTO End
     
-    :Confirmation
+:Confirmation
     ECHO System is going to Reboot now
     shutdown.exe -r -t 300
-    GOTO EOF
+    GOTO EOF   
     
-    :End
-    ECHO System Reboot cancelled
-    TIMEOUT 5 >nul
-    
-    :EOF
+:End
+    ECHO System Reboot cancelled. we will remind you in 10 minutes.
+    TIMEOUT 600
+    SET /P yesno=This computer will restart in 5 minutes. Do you want to Proceed? [Y/N]:
+    IF "%yesno%"=="y" GOTO Confirmation
+    IF "%yesno%"=="Y" GOTO Confirmation
+    IF "%yesno%"=="n" GOTO End2
+    IF "%yesno%"=="N" GOTO End2
+
+:End2
+    ECHO System Reboot cancelled. we will remind you in 10 minutes.
+    TIMEOUT 600
+    SET /P yesno=This computer will restart in 5 minutes. Do you want to Proceed? [Y/N]:
+    IF "%yesno%"=="y" GOTO Confirmation
+    IF "%yesno%"=="Y" GOTO Confirmation
+    IF "%yesno%"=="n" GOTO EOF
+    IF "%yesno%"=="N" GOTO EOF
+
+:EOF
     exit
